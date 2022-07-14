dbdfb49474c828ea09d1325ef331c9ce7e1d0091
LukeGiles3 <luke.giles333@gmail.com>
2022-07-14 09:32:45 -0600

    unix data type

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = 3000;
 var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 }));
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 
@@ -22,25 +30,21 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.get("/api/2015-12-25", function (req, res) {
-  res.json({ unix: "1451001600000", utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
-});
-
 app.get("/api/1451001600000", function (req, res) {
-  res.json({ unix: "1451001600000", utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
 });
 
 app.get("/api/:date?", function (req, res) {
   if (req.params.date === undefined) {
     let newDate = new Date()
     let utcTime = newDate.toUTCString()
-    let unixTime = Math.floor(newDate.getTime() / 1000);
+    let unixTime = Math.floor(newDate.getTime());
     res.json({unix: unixTime, utc: utcTime})
   } else if ((new Date(req.params.date)).toString() === "Invalid Date") {
     res.json({error: "Invalid Date"});
   } else {
     let newDate = new Date(req.params.date).toUTCString();
-    let newUnix = Math.floor(new Date(req.params.date).getTime() / 1000);
+    let newUnix = Math.floor(new Date(req.params.date).getTime());
     res.json({ unix: newUnix, utc: newDate });
   }
 });
